0b90948c7ab12871a5c9ed251f4eb4f1ea26ed23
pichu2707 <pichu_2707@hotmail.com>
2026-07-07 13:41:34 +0200

    Add lazarobox-ascii formula

---
+++ Formula/lazarobox-ascii.rb
@@ -0,0 +1,16 @@
+class LazaroAscii < Formula
+  desc "ASCII tool from Lazaro"
+  homepage "https://github.com/pichu2707/lazaro-ascii"
+  url "https://github.com/pichu2707/lazaro-ascii/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "e68fd3d7c93ff5ed666f136f06cdcfd48093ad9152cb6d3214c212db720df477"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+    test do
+    system "#{bin}/lazarobox-ascii", "--help"
+  end
+end
