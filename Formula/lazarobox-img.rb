767bbbf71f62f233f87ab68b86df8fa452d74fb5
Javi Lazaro <54784216+pichu2707@users.noreply.github.com>
2026-07-13 22:43:27 +0200

    lazarobox-img 0.4.3

--- Formula/lazarobox-img.rb
+++ Formula/lazarobox-img.rb
@@ -1,8 +1,8 @@
 class LazaroboxImg < Formula
   desc "Image optimizer and EXIF metadata editor with CLI and TUI"
   homepage "https://github.com/pichu2707/lazarobox-img"
-  url "https://github.com/pichu2707/lazarobox-img/archive/refs/tags/v0.4.2.tar.gz"
-  sha256 "80259dc0d2095c7764e0a146696b346fb79896c5b323162bf6b94ca0e9e7772c"
+  url "https://github.com/pichu2707/lazarobox-img/archive/refs/tags/v0.4.3.tar.gz"
+  sha256 "8cf1d4ea46e5fa29483f55fc693871225819a2f1e2e197382d76d3b31893c8bf"
   license "MIT"
 
   depends_on "rust" => :build
